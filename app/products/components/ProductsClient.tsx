7260c9c6dd60160dce45283e82226ffbaf05bad3
Sameer Alkassar <samir.alkcar@gmail.com>
2026-01-04 18:30:50 +0200

    adding suspenses to search page

---
+++ app/products/components/ProductsClient.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Products from "@/components/layout/Products";
+import { useSearchParams } from "next/navigation";
+import { CategoriesFilterType } from "@/components/ProductsSearchBar";
+
+const ProductsClient = () => {
+  const searchParams = useSearchParams();
+
+  const categoryParam = searchParams.get("category");
+
+  const category: CategoriesFilterType =
+    categoryParam === "Toys & Games" ||
+    categoryParam === "School Supplies" ||
+    categoryParam === "Gifts" ||
+    categoryParam === "All Products"
+      ? categoryParam
+      : "All Products";
+
+  const search: string = searchParams.get("search") ?? "";
+
+  return <Products search={search} category={category} />;
+};
+
+export default ProductsClient;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,31 +1,13 @@
-"use client";
-
 import Breadcrumbs from "@/components/Breadcrumbs";
-import Products from "@/components/layout/Products";
-import { useSearchParams } from "next/navigation";
-import { CategoriesFilterType } from "@/components/ProductsSearchBar";
+import ProductsClient from "./components/ProductsClient";
 import { Suspense } from "react";
 
-const ProductsPage: React.FC = () => {
-  const searchParams = useSearchParams();
-
-  const categoryParam = searchParams.get("category");
-
-  const category: CategoriesFilterType =
-    categoryParam === "Toys & Games" ||
-    categoryParam === "School Supplies" ||
-    categoryParam === "Gifts" ||
-    categoryParam === "All Products"
-      ? categoryParam
-      : "All Products";
-
-  const search: string = searchParams.get("search") ?? "";
-
+const ProductsPage = () => {
   return (
     <div className="max-w-[85rem] mx-auto pb-25">
       <Breadcrumbs currentPage="products" />
-      <Suspense fallback={<div>Loading products...</div>}>
-        <Products search={search} category={category} />
+      <Suspense fallback={<>...</>}>
+        <ProductsClient />
       </Suspense>
     </div>
   );
