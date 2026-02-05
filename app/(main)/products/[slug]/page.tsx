bb7d4acdf3dc2ea24bc8d050b91f6d954a8fa5d1
Sameer Alkassar <samir.alkcar@gmail.com>
2026-02-05 20:19:37 +0200

    generate meta data for products

--- app/(main)/products/[slug]/page.tsx
+++ app/(main)/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getProductBySlug, getRelatedBySlug } from "@/features/products/server/getProductsBySlug";
 import ProductsBySlugPage from "./components/ProductsBySlugPage";
+import type { Metadata } from "next"
 
 type PageProps = {
   params: {
@@ -10,12 +11,40 @@ type PageProps = {
 const ProductBySlug = async ({ params }: PageProps) => {
     const product = await getProductBySlug(params.slug);
     const relatedProducts = await getRelatedBySlug(params.slug, 4);
-    console.log("productsBySLug:", relatedProducts)
+    console.log("productsBySLug:", product)
     return (
         <main className="bg-ornge-50/50 px-2 lg:px-3 xl:px-0">
             <ProductsBySlugPage slug={params.slug} product={product} relatedProducts={relatedProducts}/>
         </main>
     )
 };
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const product = await getProductBySlug(params.slug)
+
+  return {
+    title: product.name,
+    description: product.description,
+    openGraph: {
+      title: product.name,
+      description: product.description,
+      images: [
+        {
+          url: product.image,
+          width: 1200,
+          height: 630,
+          alt: product.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: product.name,
+      description: product.description,
+      images: [product.image],
+    },
+  }
+};
+
 
 export default ProductBySlug;
